06525f4c111f79c2ae20b4baba5c1f3742fe3c9d
Dilan Madhusanka <dilanm@insharptechnologies.com>
2021-06-17 16:56:12 +0530

    solve the error when occur inside app

--- app/src/main/java/com/example/android_push_notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/android_push_notification/MyFirebaseMessagingService.java
@@ -1,5 +1,7 @@
 package com.example.android_push_notification;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -11,13 +13,13 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
-    @Override
-    public void onNewToken(@NonNull String token) {
-        Log.d("TAG", "The token Refreshed"+token);
-//        super.onNewToken(s);
-
-
-    }
+//    @Override
+//    public void onNewToken(@NonNull String token) {
+//        Log.d("TAG", "The token Refreshed"+token);
+////        super.onNewToken(s);
+//
+//
+//    }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
@@ -33,6 +35,17 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setAutoCancel(true);
 
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O)
+        {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel notificationChannel = new NotificationChannel("myFirebaseChannel", "NOTIFICATION_CHANNEL_NAME", importance);
+
+            builder.setChannelId("myFirebaseChannel");
+            managerCompat.createNotificationChannel(notificationChannel);
+        }
+
+
         managerCompat.notify(101, builder.build());
     }
 }
