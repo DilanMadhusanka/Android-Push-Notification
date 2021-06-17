6950d63922fb6b816a8eb6c3175b4bb0f863eb40
Dilan Madhusanka <dilanm@insharptechnologies.com>
2021-06-17 15:14:12 +0530

    push notification handle scussfully with firebase

--- app/src/main/java/com/example/android_push_notification/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/android_push_notification/MyFirebaseMessagingService.java
@@ -1,4 +1,38 @@
 package com.example.android_push_notification;
 
-public class MyFirebaseMessagingService {
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onNewToken(@NonNull String token) {
+        Log.d("TAG", "The token Refreshed"+token);
+//        super.onNewToken(s);
+
+
+    }
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        getFirebaseMessage(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+    }
+
+    public void getFirebaseMessage(String title, String msg) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "myFirebaseChannel")
+                .setSmallIcon(R.drawable.ic_baseline_notifications_active_24)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(101, builder.build());
+    }
 }
